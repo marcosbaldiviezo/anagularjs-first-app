197834eec3ce423d91f8a2e6915e44a19d6a849e
Marcos Baldiviezo <marcos.sebastian.baldiviezo@everis.com>
2019-09-16 17:51:32 -0300

    Llamadas HTTP get y post

--- controller.js
+++ controller.js
@@ -1,5 +1,43 @@
 var app = angular.module('MyFirstApp', []);
-app.controller('FirstController', function($scope) {
+app.controller('FirstController', function($scope, $http) {
+    
+
+
+
+    
+    $scope.posts = [];
+    $http.get('https://jsonplaceholder.typicode.com/posts')
+        .then(function(response) {
+            console.log(response.data);
+            $scope.posts = response.data;
+        }
+        ,function(err) {
+
+        });
+
+
+    $scope.newPost = {};    
+    $scope.addPost = function() {
+        
+        $http.post('https://cors-anywhere.herokuapp.com/https://jsonplaceholder.typicode.com/posts', {
+            title: $scope.newPost.title,
+            body: $scope.newPost.body,
+            userId: 1
+        })
+        .then(function(data, status, headers, config) {
+            console.log("regresa del post");
+            console.log(data);
+            $scope.posts.push($scope.newPost);
+            $scope.newPost = {};            
+        }
+        ,function(error, status, headers, config) {
+            console.log(error);
+        });
+    }
+
+
+
+
     $scope.nombre = 'Marcos Baldiviezo';
     $scope.nuevoComentario = {};
     $scope.comentarios = [
@@ -15,5 +53,7 @@ app.controller('FirstController', function($scope) {
     $scope.agregarComentario = function() {
         $scope.comentarios.push($scope.nuevoComentario);
         $scope.nuevoComentario = {};
-    }
+    };
+
+
 });
