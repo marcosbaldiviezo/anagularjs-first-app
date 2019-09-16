c0be655065d600dea21c177d52142765e0756fd5
Marcos Baldiviezo <marcos.sebastian.baldiviezo@everis.com>
2019-09-16 14:47:19 -0300

    Extendiendo el controlador

--- controller.js
+++ controller.js
@@ -1,4 +1,19 @@
 var app = angular.module('MyFirstApp', []);
 app.controller('FirstController', function($scope) {
     $scope.nombre = 'Marcos Baldiviezo';
-})
+    $scope.nuevoComentario = {};
+    $scope.comentarios = [
+        {
+            comentario: 'Buen tutorial',
+            username: 'marcos'
+        },
+        {
+            comentario: 'Mal tutorial',
+            username: 'user1'
+        }
+    ];
+    $scope.agregarComentario = function() {
+        $scope.comentarios.push($scope.nuevoComentario);
+        $scope.nuevoComentario = {};
+    }
+});
